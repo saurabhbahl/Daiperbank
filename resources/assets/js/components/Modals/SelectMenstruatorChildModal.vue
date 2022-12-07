eb3175e35a335af992d72f42fbed0521aa8af766
Anirudh-Chopra <anirudhchopra17@gmail.com>
2022-12-07 22:01:27 +0530

    Code Changes related to Child list filter

--- resources/assets/js/components/Modals/SelectMenstruatorChildModal.vue
+++ resources/assets/js/components/Modals/SelectMenstruatorChildModal.vue
@@ -45,7 +45,7 @@ export default {
 
 	data() {
 		return {
-			Children: this.allChildren,
+			Children: this.allChildren.filter(x=>x.is_menstruator==1),
 			text: "Menstruator",
 			selected_child_id: null,
 		}
