894788ab92f687adff90c5eb2e1b4aa9c251528c
Saurabh Behl <behl.saurabh@gmail.com>
2023-08-23 13:53:42 +0530

    Update period products items

--- resources/assets/js/components/General/ProductSelector.vue
+++ resources/assets/js/components/General/ProductSelector.vue
@@ -42,7 +42,7 @@
 						@change="onProductChange">
 						<option disabled>Select one</option>
 						<option v-for="Product in CategoryProducts"
-								:value="Product.id" v-if="Product.id != 17 && Product.id != 18 && Product.id != 19 && Product.id != 20">
+								:value="Product.id" >
 								{{ Product.name }}
 						</option>
 					</select>
