d722f348fcb8d634f53df65858f8e217ddcbc5a5
Saurabh Behl <behl.saurabh@gmail.com>
2023-09-05 13:00:07 +0530

    Testing

--- resources/assets/js/components/Orders/ChildDetail.vue
+++ resources/assets/js/components/Orders/ChildDetail.vue
@@ -81,7 +81,6 @@
         </div>
 
         <p class="b f2 bb bw2 b--black-20 mv3">Current Order</p>
-        <p class="b f2 bb bw2 b--black-20 mv3" v-if="Child.child.order_count >= 6">You can place only 6 orders for pull ups.</p>
         <table class="table table-condensed table-striped" v-if="!isEditing">
           <thead>
             <tr>
@@ -114,7 +113,7 @@
               {{ msg }}
             </p>
           </div>
-
+          <p class="b f2 bb bw2 b--black-20 mv3" v-if="Child.child.order_count >= 6">You can place only 6 orders for pull ups.</p>
           <ProductSelector
             :product-categories="productCategories"
             :initial-selected-product="selectedProductId"
